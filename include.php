b24955c9e92306ea4dfe90d8193ab4f1d720076d
dagrenzer <daryl.grenz@kaust.edu.sa>
2021-05-04 10:42:53 +0300

    Make sure all files use unix line endings instead of dos

--- include.php
+++ include.php
@@ -2,7 +2,7 @@
 	// change it to 0 when moving to production
 	ini_set('display_errors', 1);
 
-	//subdirectories must be added explicitly so that directories holding test files are not included unintentionally
+	//subdirectories are added explicitly so that directories holding test files are not included unintentionally
 	$directoriesToInclude = array("config" , "functions/admin", "functions/dspace", "functions/local", "functions/orcid", "functions");
 
 	foreach($directoriesToInclude as $directory)
